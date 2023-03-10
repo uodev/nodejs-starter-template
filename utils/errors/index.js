1e11a0dfbb4924accd405af5d37d20b823479c37
uodev <uygaroztcyln@gmail.com>
2023-03-10 13:12:00 +0300

    add new feature

---
+++ utils/errors/index.js
@@ -0,0 +1,13 @@
+const errors = {
+  NotFound: 404,
+  BadRequest: 400,
+  Unauthorized: 401,
+  Forbidden: 403,
+  Conflict: 409,
+  InternalServerError: 500,
+  NoContent: 204,
+  Created: 201,
+  Ok: 200,
+};
+
+module.exports = errors;
--- utils/response/index.js
+++ utils/response/index.js
@@ -1,61 +1,63 @@
+const status = require("../errors/index");
+
 const Ok = (res, data) => {
-  return res.status(200).json({
+  return res.status(status.Ok).json({
     success: true,
     data: data || {},
   });
 };
 
 const Created = (res, data) => {
-  return res.status(201).json({
+  return res.status(status.Created).json({
     success: true,
     data: data || {},
   });
 };
 
 const NoContent = (res) => {
-  return res.status(204).json({
+  return res.status(status.NoContent).json({
     success: true,
     data: {},
   });
 };
 
 const BadRequest = (res, message) => {
-  return res.status(400).json({
+  return res.status(status.BadRequest).json({
     success: false,
     message: message || "Bad Request",
   });
 };
 
 const Unauthorized = (res, message) => {
-  return res.status(401).json({
+  return res.status(status.Unauthorized).json({
     success: false,
     message: message || "Unauthorized",
   });
 };
 
 const Forbidden = (res, message) => {
-  return res.status(403).json({
+  return res.status(status.Forbidden).json({
     success: false,
     message: message || "Forbidden",
   });
 };
 
 const NotFound = (res, message) => {
-  return res.status(404).json({
+  return res.status(status.NotFound).json({
     success: false,
     message: message || "Not Found",
   });
 };
 
 const Conflict = (res, message) => {
-  return res.status(409).json({
+  return res.status(status.Conflict).json({
     success: false,
     message: message || "Conflict",
   });
 };
 
 const InternalServerError = (res, message) => {
-  return res.status(500).json({
+  return res.status(status.InternalServerError).json({
     success: false,
     message: message || "Internal Server Error",
   });
